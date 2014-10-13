2e261cfdee7f52dbe795f5cd82d259a65fb13057
Brentenbrood <brentminkes@hotmail.com>
2014-10-13 09:40:40 +0200

    fuck git

--- js/inputHandler.js
+++ js/inputHandler.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
 	$('.iconclose').click(function(){
-		
+		var parent1 = $(this).parent();
+		$(parent1).parent().css({display:'none'});
 	});
 	$('#cmd').draggable({
 		handle: ".dragbar",
 		containment:"window",
-		start: function(event, ui) 
-		       { $(this).css("z-index", 1); }
+		stack: "div"
 		});
 	$('#time').draggable({
 		handle: ".dragbar",
 		containment:"window",
-		start: function(event, ui) { $(this).css("z-index", 1); }
+		stack: "div"
 		});
+	function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    $('#clock span').text(h+":"+m+":"+s).css({});
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};
+    return i;
+}
+startTime();
 });
 
